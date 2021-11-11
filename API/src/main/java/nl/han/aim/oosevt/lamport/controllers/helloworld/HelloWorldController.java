f91927bf418dc130672984b657b4e5e4d41a6481
BartBarends <bart.barends.bb@gmail.com>
2021-11-11 13:08:23 +0100

    CORS

--- API/src/main/java/nl/han/aim/oosevt/lamport/controllers/helloworld/HelloWorldController.java
+++ API/src/main/java/nl/han/aim/oosevt/lamport/controllers/helloworld/HelloWorldController.java
@@ -5,10 +5,7 @@ import nl.han.aim.oosevt.lamport.services.helloworld.IHelloWorldService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/helloworld")
