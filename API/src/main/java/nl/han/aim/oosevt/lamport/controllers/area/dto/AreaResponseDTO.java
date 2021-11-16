ff6ee1b1945c11f928ef977e7a4c6f7d86def3f8
Wesley Zuidema (639292) <W.Zuidema@student.han.nl>
2021-11-16 10:52:51 +0100

    Implementatie AreaServiceImpl.getAreas(), DAO interface gemaakt met .getAreas(), DAO interface realisatie klasse met stub methode voor Autowired

--- API/src/main/java/nl/han/aim/oosevt/lamport/controllers/area/dto/AreaResponseDTO.java
+++ API/src/main/java/nl/han/aim/oosevt/lamport/controllers/area/dto/AreaResponseDTO.java
@@ -5,13 +5,15 @@ import nl.han.aim.oosevt.lamport.data.entity.Area;
 
 public class AreaResponseDTO extends GeoFenceResponseDTO {
     private int id;
+    private String name;
 
     public AreaResponseDTO() {
     }
 
-    public AreaResponseDTO(int id, double longitude, double latitude, int radius) {
+    public AreaResponseDTO(int id, String name, double longitude, double latitude, int radius) {
         super(longitude, latitude, radius);
         this.id = id;
+        this.name = name;
     }
 
     public int getId() {
@@ -22,9 +24,20 @@ public class AreaResponseDTO extends GeoFenceResponseDTO {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public AreaResponseDTO fromData(Area area) {
         //todo implement
         //Bart - 15/11/2021
+
+        //todo generics
+        //Wesley - 16/11/2021
         return this;
     }
 }
---
+++ API/src/main/java/nl/han/aim/oosevt/lamport/data/dao/area/AreaDAO.java
@@ -0,0 +1,10 @@
+package nl.han.aim.oosevt.lamport.data.dao.area;
+
+import nl.han.aim.oosevt.lamport.data.entity.Area;
+
+import java.util.List;
+
+public interface AreaDAO {
+
+    List<Area> getAreas();
+}
