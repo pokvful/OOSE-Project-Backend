d3f69787542773b4e8d8db19167ddf85e8c8d76b
Ard-Jan van Etten <ardjanve@gmail.com>
2021-11-16 11:14:13 +0100

    UC8 service create tests

---
+++ API/src/test/java/nl/han/aim/oosevt/lamport/services/location/TestLocationServiceImpl.java
@@ -0,0 +1,58 @@
+package nl.han.aim.oosevt.lamport.services.location;
+
+import nl.han.aim.oosevt.lamport.data.dao.location.LocationDAO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class TestLocationServiceImpl {
+    private final String name = "mcDonalds";
+    private final int delay = 10;
+    private final double longitude = 4.32132;
+    private final double latitude = 51.32132431;
+    private final int radius = 30;
+
+    private LocationServiceImpl sut;
+    private LocationDAO locationDAOFixture;
+    private CreateLocationRequestDTO createLocationRequestDTO;
+
+    @BeforeEach
+    public void setup() {
+        realCreateLocationRequestDTO = new CreateLocationRequestDTO();
+        createLocationRequestDTO = Mockito.spy(realCreateLocationRequestDTO);
+
+        createLocationRequestDTO.setName(name);
+        createLocationRequestDTO.setDelay(delay);
+        createLocationRequestDTO.setLongitude(longitude);
+        createLocationRequestDTO.setLatitude(latitude);
+        createLocationRequestDTO.setRadius(radius);
+
+        locationDAOFixture = Mockito.mock(LocationDAO.class);
+
+        sut = new LocationServiceImpl(locationDAOFixture);
+    }
+
+    @Test
+    public void testCreateLocationVerifies() {
+        // Act
+        sut.createLocation(createLocationRequestDTO);
+
+        // Assert
+        Mockito.verify(createLocationRequestDTO).verify();
+    }
+
+    @Test
+    public void testCreateLocationCallsDB() {
+        // Act
+        sut.createLocation(createLocationRequestDTO);
+
+        // Assert
+        Mockito.verify(locationDAOFixture).createLocation(
+            name,
+            delay,
+            longitude,
+            latitude,
+            radius
+        );
+    }
+}
