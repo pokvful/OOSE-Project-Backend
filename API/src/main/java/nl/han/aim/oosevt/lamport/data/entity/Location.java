3540f990de7c37cbb6cf3975110e4eecee38f0aa
Tim <T.Meuwsen@student.han.nl>
2021-11-17 14:12:46 +0100

    made entity props final

--- API/src/main/java/nl/han/aim/oosevt/lamport/data/entity/Location.java
+++ API/src/main/java/nl/han/aim/oosevt/lamport/data/entity/Location.java
@@ -2,16 +2,13 @@ package nl.han.aim.oosevt.lamport.data.entity;
 
 public class Location {
 
-    private int id;
-    private String name;
-    private int delay;
-    private double longitude;
-    private double latitude;
-    private int radius;
-    private int areaId;
-
-    public Location() {
-    }
+    final private int id;
+    final private String name;
+    final private int delay;
+    final private double longitude;
+    final private double latitude;
+    final private int radius;
+    final private int areaId;
 
     public Location(int id, String name, int delay, double longitude, double latitude, int radius, int areaId) {
         this.id = id;
@@ -27,55 +24,27 @@ public class Location {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public int getDelay() {
         return delay;
     }
 
-    public void setDelay(int delay) {
-        this.delay = delay;
-    }
-
     public double getLongitude() {
         return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
     }
 
     public double getLatitude() {
         return latitude;
     }
 
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
     public int getRadius() {
         return radius;
     }
 
-    public void setRadius(int radius) {
-        this.radius = radius;
-    }
-
     public int getAreaId() {
         return areaId;
-    }
-
-    public void setAreaId(int areaId) {
-        this.areaId = areaId;
     }
 }
