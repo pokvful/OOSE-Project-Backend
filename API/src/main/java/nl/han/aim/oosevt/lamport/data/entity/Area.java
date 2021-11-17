98fb4cf7a34ca71b0466704998d593947d4851a8
Wesley Zuidema (639292) <W.Zuidema@student.han.nl>
2021-11-17 11:12:39 +0100

    * Area database entity for the purpose of getting Area objects from database rows.

--- API/src/main/java/nl/han/aim/oosevt/lamport/data/entity/Area.java
+++ API/src/main/java/nl/han/aim/oosevt/lamport/data/entity/Area.java
@@ -1,4 +1,59 @@
 package nl.han.aim.oosevt.lamport.data.entity;
 
 public class Area {
+
+    private int id;
+    private String name;
+    private double latitude;
+    private double longitude;
+    private int radius;
+
+    public Area(int id, String name, double latitude, double longitude, int radius) {
+        this.id = id;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.radius = radius;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
 }
