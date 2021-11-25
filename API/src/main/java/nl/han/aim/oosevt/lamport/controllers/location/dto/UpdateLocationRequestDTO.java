bea0a4cf69d9ed99f1dbda1224a5562284317357
Ard-Jan van Etten <ardjanve@gmail.com>
2021-11-25 13:12:48 +0100

    refactor area unittests

--- API/src/main/java/nl/han/aim/oosevt/lamport/controllers/location/dto/UpdateLocationRequestDTO.java
+++ API/src/main/java/nl/han/aim/oosevt/lamport/controllers/location/dto/UpdateLocationRequestDTO.java
@@ -3,27 +3,27 @@ package nl.han.aim.oosevt.lamport.controllers.location.dto;
 import java.util.List;
 
 public class UpdateLocationRequestDTO extends LocationRequestDTO {
-    private int locationId;
+    private int id;
 
     public UpdateLocationRequestDTO() {
     }
 
-    public UpdateLocationRequestDTO(int locationId, String name, int delay, double longitude, double latitude, int radius, int areaId, List<Integer> linkedInterventions) {
+    public UpdateLocationRequestDTO(int id, String name, int delay, double longitude, double latitude, int radius, int areaId, List<Integer> linkedInterventions) {
         super(name, delay, longitude, latitude, radius, areaId, linkedInterventions);
-        this.locationId = locationId;
+        this.id = id;
     }
 
-    public int getLocationId() {
-        return locationId;
+    public int getId() {
+        return id;
     }
 
-    public void setLocationId(int locationId) {
-        this.locationId = locationId;
+    public void setId(int id) {
+        this.id = id;
     }
 
     @Override
     protected void validateSpecificDto() {
-        if(locationId == 0) {
+        if(id == 0) {
             addError("id", "Id mag niet leeg zijn");
         }
     }
